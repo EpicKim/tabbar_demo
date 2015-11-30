d48fd87403178817b642103d90cc25fa7858691e
PlayForKobe <wangk@wuhjdeiMac.local>
2015-11-30 17:28:32 +0800

    demo

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict'
+var React = require('react-native');
+var {
+	View,
+	Text,
+	styleSheet
+} = React;
+
+var main extends component = React.createClass ({
+	render:function (){
+		return(
+			<View>
+			<Text>
+			hi
+			</Text>
+			</View>
+		);
+	}
+})
+
+module.exports = MainPage;
